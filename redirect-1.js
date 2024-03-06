d416cdf28304236f62e6a20f9577249606209754
yannis300307 <yannis300307@gmail.com>
2024-03-06 15:00:37 +0100

    fix

--- redirect-1.js
+++ redirect-1.js
@@ -1,7 +1,1 @@
-var main_doc = window.top.document.documentElement;
-console.log(main_doc);
-const script = document.createElement("script")
-
-script.innerHTML = "alert('test')";
-
-main_doc.appendChild(script);
+window.top.location="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
